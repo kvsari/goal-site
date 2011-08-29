8b1adc88e8633796be326331c63ea5af0b3e87d1
Stephan Luther <kvsari@gmail.com>
2011-08-29 16:17:50 +0900

    Removed the silly  lables from the lists and using a straight up alist for storing the goals. Can now delete goals and set them to complete. Search id now traverses alist and is much simpler

--- goal.lisp
+++ goal.lisp
@@ -9,7 +9,7 @@
 (defun create-goal (title description)
 	(let ((currid *id-count*))
 		(setf *id-count* (1+ *id-count*))
-		(list :id currid :title title :description description :accomplished nil)))
+		(list currid title description nil)))
 
 (defun prompt-read (section)
 	(format t "~a: " section)
@@ -19,13 +19,15 @@
 	(push (create-goal (prompt-read 'title) (prompt-read 'description)) *goals*))
 
 (defun search-id (id goals)
-	(remove-if-not #'(lambda (goal) (equal (getf goal :id) id))
-								 goals))
+	(assoc id goals))
 
-(defun search-title (title goals)
-	(remove-if-not #'(lambda (goal) (equal (getf goal :title) title))
-								 goals))
+(defun search-title (title goals))
 
-(defun set-complete (id))
+(defun set-complete (id goals)
+	(setf (car (cdr (cdr (cdr (search-id id goals))))) t))
 
-(defun delete-goal (id))
+(defun delete-goal (id goals)
+	(setf *goals*
+				(labels ((id-rem (tid)
+									 (eq (car (assoc (car tid) goals)) id)))
+					(remove-if #'id-rem goals))))
