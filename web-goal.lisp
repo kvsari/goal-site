acb78782366bcee4f97733bf025a879e5c40f8ef
Stephan Luther <kvsari@gmail.com>
2011-09-05 20:27:29 +0900

    Created those nice rounded CSS boxes for goal data and the form. Boxes are quite crude though. Can list all goals on site. Have begun creating a minature form for each goal with a button to accomplish or delete the goal. Currently this is not working. There is some bizzare behaviour being exhibited which will require a good debugging session. For some reason even though the delete action is being captured with the correct goal id it doesn't save the changes.

--- web-goal.lisp
+++ web-goal.lisp
@@ -16,9 +16,13 @@
 											 (princ "Goal Site"))
 									(embed-css3))
 						 (tag body ()
-									(princ "Goal Site!<br>")
+									(tag h2 ()
+											 (princ "Goal Site!<br>"))
 									(process-parameters params)
-									(create-goal-form)))
+									(create-goal-form)
+									(tag h3 ()
+											 (princ "All Goals<br>"))
+									(list-modifiable-goals *goals*)))
 				(save-goals-to-file "test.goals" *goals*))))
 
 (defun html5-doctype ()
@@ -27,12 +31,18 @@
 ; Currently only handles form input for adding goals.
 (defun process-parameters (params)
 	(if params
-			(if (equal (car (assoc 'createGoalTitle params)) 'createGoalTitle)
-					(progn
-						(process-create-goal-parameters params)
-						(tag script ()
-								 (princ "window.setTimeout('window.location=\"goals\"',100)"))); Clear out the address bar of parameters
-					)))
+			(progn
+				(if (equal (car (assoc 'createGoalTitle params)) 'createGoalTitle)
+						(process-create-goal-parameters params))
+				(if (equal (car (assoc 'delete params)) 'delete)
+						;(error (cdr (assoc 'goalid params)))
+						(delete-goal (cdr (assoc 'goalid params)))
+						(error "Not deleting"))
+				;(if (equal (car (assoc 'accomplished params)) 'accomplished)
+				;		(set-complete (cdr (assoc 'goalid params)) *goals*))
+				(tag script ()
+						 (princ "window.setTimeout('window.location=\"goals\"',1000)"))); Clear out the address bar of parameters
+			))
 
 (defun process-create-goal-parameters (params)
 	(let ((title (cdr (assoc 'createGoalTitle params)))
@@ -40,8 +50,8 @@
 		(set-goal title desc)))
 
 (defun create-goal-form ()
-	(tag form (method 'post)
-			 (tag h3
+	(tag form (method 'post id 'creategoalform)
+			 (tag h3 ()
 						(princ "Create Goal"))
 			 (tag p ()
 						(tag label (for 'name)
@@ -55,13 +65,60 @@
 (defun embed-css3 ()
 	(tag style (type "text/css")
 			 (progn
+				 (format t "~%")
 				 (princ "{")
 				 (princ "margin: 0;")
 				 (princ "padding: 0;")
 				 (princ "}")
 				 (format t "~%~%")
-				 (princ "header, footer, aside, nav, article {  display: block;}"))))
+				 (princ "header, footer, aside, nav, article {  display: block;}")
+				 (format t "~%~%")
+				 (princ "#CREATEGOALFORM {")
+				 (princ "background-color: red;")
+				 (princ "border-radius: 22px;")
+				 (princ "padding: 25px;")
+				 (princ "}")
+				 (format t "~%~%")
+				 (princ "#GOALINFO {")
+				 (princ "background-color: green;")
+				 (princ "border-radius: 22px;")
+				 (princ "padding: 25px;")
+				 (princ "}")
+				 (format t "~%~%")
+				 (princ "#EXPANDEDGOALINFO {")
+				 (princ "background-color: yellow;")
+				 (princ "border-radius: 22px;")
+				 (princ "padding: 25px;")
+				 (princ "}")
+				 )))
 
-(defun goal-info ())
+(defun list-goals (goals)
+	(mapcar #'goal-info goals))
 
-(defun expanded-goal-info ())
+(defun list-modifiable-goals (goals)
+	(mapcar #'modifiable-goal-info goals))
+
+(defun goal-info (goal)
+	(tag section (id 'goalinfo)
+			 (tag h4 ()
+						(format t "~a" (car (cdr goal))))
+			 (tag p ()
+						(format t "~a" (car (cdr (cdr goal)))))
+			 (tag p ()
+						(progn
+							(princ "Status: ")
+							(if (eq (car (cdr (cdr (cdr goal)))) nil)
+									(princ "Incomplete")
+									(princ "Achieved"))))))
+
+(defun modifiable-goal-info (goal)
+	(tag section (id 'expandedgoalinfo)
+			 (progn
+				 (goal-info goal)
+				 (tag form (method 'post)
+							(progn
+								(tag input (type 'hidden name 'goalid value (car goal)))
+								(tag input (type 'submit name 'accomplished value 'Accomplish))
+								(tag input (type 'hidden name 'goalid value (car goal)))
+								(tag input (type 'submit name 'delete value 'delete)))))))
+							
