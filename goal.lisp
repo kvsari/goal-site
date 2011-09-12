4c58f9dc7f3e488d9f5661b42f12de58f1d94481
Stephan Luther <kvsari@gmail.com>
2011-09-12 22:03:55 +0900

    Added in todo items. They are functionally identical to goals except they only contain a single item discription istead of a title and description and they are timestamped.

--- goal.lisp
+++ goal.lisp
@@ -4,12 +4,22 @@
 
 (defparameter *goals* nil)
 
+(defparameter *todo-list* nil)
+
 (defparameter *id-count* 0)
+
+(defparameter *todo-count* 0)
 
 (defun create-goal (title description)
 	(let ((currid *id-count*))
 		(setf *id-count* (1+ *id-count*))
 		(list currid title description nil)))
+
+(defun create-todo-item (item)
+	(let ((currid *todo-count*)
+				(timestamp (get-universal-time)))
+		(setf *todo-count* (1+ *todo-count*))
+		(list currid item timestamp nil)))
 
 (defun prompt-read (section)
 	(format t "~a: " section)
@@ -18,8 +28,14 @@
 (defun set-goal (title description)
 	(push (create-goal title description) *goals*))
 
+(defun set-todo (item)
+	(push (create-todo-item item) *todo-list*))
+
 (defun set-goal-repl ()
 	(push (create-goal (prompt-read 'title) (prompt-read 'description)) *goals*))
+
+(defun set-todo-repl ()
+	(push (create-todo-item (prompt-read 'item)) *todo-list*))
 
 (defun search-id (id goals)
 	(assoc id goals))
@@ -30,6 +46,9 @@
 (defun set-complete (id goals)
 	(setf (car (cdr (cdr (cdr (search-id id goals))))) t))
 
+(defun set-todo-item-complete (id item)
+	(setf (car (cdr (cdr (cdr (search-id id item))))) t))
+
 (defun delete-goal (id)
 	(setf *goals*
 				(let ((goals *goals*))
@@ -37,12 +56,26 @@
 										 (eq (car (assoc (car tid) goals)) id)))
 						(remove-if #'id-rem goals)))))
 
+(defun delete-todo-item (id)
+	(setf *todo-list*
+				(let ((items *todo-list*))
+					(labels ((id-rem (tid)
+										 (eq (car (assoc (car tid) items)) id)))
+						(remove-ifx #'id-rem items)))))
+
 (defun save-goals-to-file (filename goals)
 	(with-open-file (out filename
 											 :direction :output
 											 :if-exists :supersede)
 		(with-standard-io-syntax
 			(print goals out))))
+
+(defun save-todo-items-to-file (filename items)
+	(with-open-file (out filename
+											 :direction :output
+											 :if-exists :supersede)
+		(with-standard-io-syntax
+			(print items out))))
 
 (defun load-goals-from-file (filename)
 	(progn
@@ -52,6 +85,15 @@
 				(setf *goals* (read in))))
 		(let ((goals *goals*))
 			(setf *id-count* (1+ (car (car goals))))))) ; Last goal has the highest id
+
+(defun load-todo-items-from-file (filename)
+	(progn
+		(with-open-file (in filename
+												:direction :input)
+			(with-standard-io-syntax
+				(setf *todo-list* (read in))))
+		(let ((items *todo-list*))
+			(setf *todo-count* (1+ (car (car items)))))))
 
 (defun list-all-goals (goals)
 	(labels ((print-goal (goal)
