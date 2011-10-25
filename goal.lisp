cc2747d48b0b8727507be6de859ba92b9b9d944b
Stephan Luther <kvsari@gmail.com>
2011-10-25 10:55:18 +0900

    Added in processing of todo items. Can create and add, delete or set complete todo items

--- goal.lisp
+++ goal.lisp
@@ -34,11 +34,19 @@
 				(body (list title description)))
 		(list header body)))
 
+;;; Can be macro'd
 (defun add-goal (goal data)
 	(let ((goals (getf data :goals)))
 		(if goals
 				(push goal (getf data :goals))
 				(setf (getf data :goals) (list goal)))))
+
+;;; ditto
+(defun add-todo-item (item data)
+	(let ((items (getf data :todo-list)))
+		(if items
+				(push item (getf data :todo-list))
+				(setf (getf data :todo-list) (list item)))))
 
 ;;; Returns a list containing the correct header id. Expects a list of lists containing headers as their first element
 (defun search-id (id data)
@@ -49,6 +57,7 @@
 		 (remove-if-not #'narrow data))))
 
 ;;; Must feed it the *information* global for data or an equally laid out data source
+;;; This and below are candidates for macroisation
 (defun delete-goal (id data)
 	(setf (getf data :goals)
 				(let ((goals (getf data :goals)))
@@ -57,11 +66,21 @@
 											 (equal (car header) id))))
 						(remove-if #'test goals)))))
 
+;;; Must feed it the *information* global for data or an equally laid out data source
+;;; This and above can be turned into a macro.
+(defun delete-todo-item (id data)
+	(setf (getf data :todo-list)
+				(let ((items (getf data :todo-list)))
+					(labels ((test (segment)
+										 (let ((header (car segment)))
+											 (equal (car header) id))))
+						(remove-if #'test items)))))
+
 ;;; Adds an extra field in the header portion of the segment with the value T. Expecting the header portion to only have
 ;;; two elements in the list since it will overwrite any third element. Expects data to be a list of lists that contain
 ;;; a header as the first item
 (defun set-complete (id data)
-	(let ((goal (search-goal-id id data)))
+	(let ((goal (search-id id data)))
 		(if goal
 				(setf (cdr (cdr (car goal))) (list t)))))
 
@@ -70,11 +89,14 @@
 	(let ((goal (create-goal (get-next-id (getf *information* :goals)) (prompt-read 'Title) (prompt-read 'Description))))
 		(add-goal goal *information*)))
 
-(defun create-todo-item (item)
-	(let ((currid *todo-count*)
-				(timestamp (get-universal-time)))
-		(setf *todo-count* (1+ *todo-count*))
-		(list currid item timestamp nil)))
+(defun add-todo-item-repl ()
+	(let ((item (create-todo-item (get-next-id (getf *information* :todo-list)) (prompt-read 'Item-Text))))
+		(add-todo-item item *information*)))
+
+(defun create-todo-item (id item)
+	(let ((header (create-header id))
+				(body (list item)))
+		(list header body)))
 
 (defun prompt-read (section)
 	(format t "~a: " section)
@@ -106,33 +128,9 @@
 											 (let ((header (car segment)))
 												 (equal (car header) id))))
 							(remove-if #'test notes)))))))
-			
-(defun set-todo (item)
-	(push (create-todo-item item) *todo-list*))
-
-(defun set-todo-repl ()
-	(push (create-todo-item (prompt-read 'item)) *todo-list*))
 
 ;;; This particular function is not in the iteration 1 spec so will leave as stub for now
 (defun search-title (title goals))
-
-(defun set-todo-item-complete (id item)
-	(setf (car (cdr (cdr (cdr (search-id id item))))) t))
-
-(defun delete-todo-item (id)
-	(setf *todo-list*
-				(let ((items *todo-list*))
-					(labels ((id-rem (tid)
-										 (eq (car (assoc (car tid) items)) id)))
-						(remove-if #'id-rem items)))))
-
-(defun list-all-goals (goals)
-	(labels ((print-goal (goal)
-						 (format t "ID: ~a~%" (car goal))
-						 (format t "Title: ~a~%" (car (cdr goal)))
-						 (format t "Description: ~a~%" (car (cdr (cdr goal))))
-						 (format t "Accomplished: ~a~%~%" (car (cdr (cdr (cdr goal)))))))
-		(mapcar #'print-goal goals)))
 
 (defun save-information-to-file (filename information)
 	(with-open-file (out filename
